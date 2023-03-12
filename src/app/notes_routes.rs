5c95eccb628cce3ac283d02e5ca4886da9b063bb
Logan Keenan <d.logankeenan@gmail.com>
2023-03-11 21:58:57 -0600

    add basic askama example

--- src/app/notes_routes.rs
+++ src/app/notes_routes.rs
@@ -1,7 +1,11 @@
-use axum::response::{Html, IntoResponse};
+use askama_axum::Template;
 use axum_cloudflare_adapter::{worker_route_compat};
 
+#[derive(Template)]
+#[template(path = "notes/index.html")]
+pub struct IndexTemplate {}
+
 #[worker_route_compat]
-pub async fn index() -> impl IntoResponse {
-	Html("hello world!")
+pub async fn index() -> IndexTemplate {
+		IndexTemplate {}
 }
